00fe1bc586f60cddf2826a41e1de910d4dce47cf
krazynicko <nicogeta11@gmail.com>
2024-09-28 15:43:38 -0300

    se actualizo settings.py con middlware de cache

--- proyecto_web/settings.py
+++ proyecto_web/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware'
 ]
 
 ROOT_URLCONF = 'proyecto_web.urls'
